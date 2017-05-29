43ea8ab63139d2d4adaf3d8538312070b02109ce
ncomptier <ncomptier@e-biz.fr>
2017-05-29 11:43:27 +0200

    [REST API] WIP Computer rest api (Jackson, Spring RestController)

---
+++ webapp/src/main/java/com/ebiz/cdb/api/CompanyRestController.java
@@ -0,0 +1,4 @@
+package com.ebiz.cdb.api;
+
+public class CompanyRestController {
+}
---
+++ webapp/src/main/java/com/ebiz/cdb/api/ComputerRestController.java
@@ -0,0 +1,191 @@
+package com.ebiz.cdb.api;
+
+import com.ebiz.cdb.binding.dto.ComputerDTO;
+import com.ebiz.cdb.binding.mapper.ComputerMapper;
+import com.ebiz.cdb.binding.validator.ComputerDTOValidator;
+import com.ebiz.cdb.core.models.Computer;
+import com.ebiz.cdb.service.CompanyService;
+import com.ebiz.cdb.service.ComputerService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/computer")
+public class ComputerRestController {
+
+    public final String BaseLocation = "/api/computer";
+
+    private org.slf4j.Logger LOGGER = LoggerFactory.getLogger("com.ebiz.cdb.api.ComputerRestController");
+
+    private final CompanyService companyService;
+    private final ComputerService computerService;
+    private final ComputerDTOValidator computerDTOValidator;
+    private final ComputerMapper computerMapper;
+
+    /**
+     * ComputerRestController constructor.
+     *
+     * @param companyService       autowired companyService
+     * @param computerService      autowired computerService
+     * @param computerDTOValidator autowired computerDTOValidator
+     * @param computerMapper       autowired computerMapper
+     */
+    @Autowired
+    public ComputerRestController(CompanyService companyService, ComputerService computerService, ComputerDTOValidator computerDTOValidator, ComputerMapper computerMapper) {
+        this.companyService = companyService;
+        this.computerService = computerService;
+        this.computerDTOValidator = computerDTOValidator;
+        this.computerMapper = computerMapper;
+    }
+
+
+    /**
+     * Get computer by id.
+     *
+     * @param id computer id
+     * @return List of computer
+     */
+    @GetMapping("/getById/{id}")
+    public Computer getById(@PathVariable("id") Long id) {
+        LOGGER.info("[GET rest] get computer by id");
+        return computerService.findById(id);
+    }
+
+    /**
+     * Get computer by id.
+     *
+     * @param name             computer name
+     * @param page             page number.
+     * @param nbComputerByPage number of computers displayed by page.
+     * @return List of computer
+     */
+    @GetMapping("/getByName/{name}")
+    public List<Computer> getByName(@PathVariable("name") String name, @PathVariable("page") int page, @PathVariable("nbComputerByPage") int nbComputerByPage) {
+        LOGGER.info("[GET rest] get computer by name or company name");
+        return computerService.findByName(name, page, nbComputerByPage);
+    }
+
+    /**
+     * Get all computers by page.
+     *
+     * @param page             page number.
+     * @param nbComputerByPage number of computers displayed by page.
+     * @return computer list by page.
+     */
+    @GetMapping("/getPage/{page}/{nbComputerByPage}")
+    public List<Computer> getByPage(@PathVariable("page") int page, @PathVariable("nbComputerByPage") int nbComputerByPage) {
+        LOGGER.info("[GET rest] get computers by page");
+        return computerService.getByPage(page, nbComputerByPage);
+    }
+
+    /**
+     * Count all computers.
+     *
+     * @return number of computers.
+     */
+    @GetMapping("/count")
+    public Long count() {
+        LOGGER.info("[GET rest] count computers");
+        return computerService.count();
+    }
+
+    /**
+     * Count computers by name or company name.
+     *
+     * @param name computer or company name.
+     * @return number of computers.
+     */
+    @GetMapping("/count/{name}")
+    public Long countByName(@PathVariable("name") String name) {
+        LOGGER.info("[GET rest] count computers ny name");
+        return computerService.countByName(name);
+    }
+
+    /**
+     * Get all computers.
+     *
+     * @return List of computer
+     */
+    @GetMapping("/all")
+    public List<Computer> getAll() {
+        LOGGER.info("[GET rest] get all computers");
+        return computerService.getAll();
+    }
+
+    /**
+     * Create computer.
+     *
+     * @param computerDTO computerDTO.
+     * @param result      result.
+     * @return computer id.
+     * @throws MalformedURLException MalformedURLException
+     * @throws URISyntaxException    URISyntaxException
+     */
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody @Validated ComputerDTO computerDTO, BindingResult result) throws MalformedURLException, URISyntaxException {
+        LOGGER.info("[GET rest] create computer");
+        String location = BaseLocation + "/create";
+        URL url = new URL(location);
+        URI uri = url.toURI();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(uri);
+        if (!result.hasErrors()) {
+            return new ResponseEntity<Long>(computerService.create(computerMapper.to(computerDTO)), responseHeaders, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<Integer>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Update computer.
+     *
+     * @param computerDTO computerDTO.
+     * @param result      result.
+     * @return computer id.
+     * @throws MalformedURLException MalformedURLException
+     * @throws URISyntaxException    URISyntaxException
+     */
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@RequestBody @Validated ComputerDTO computerDTO, BindingResult result) throws MalformedURLException, URISyntaxException {
+        LOGGER.info("[GET rest] update computer");
+        String location = BaseLocation + "/update";
+        URL url = new URL(location);
+        URI uri = url.toURI();
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(uri);
+        if (!result.hasErrors()) {
+            return new ResponseEntity<Long>(computerService.update(computerMapper.to(computerDTO)), responseHeaders, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<Integer>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Delete computer.
+     *
+     * @param id computer id
+     * @return computer id.
+     */
+    @PostMapping("/delete/{id}")
+    public Long delete(@PathVariable("id") int id) {
+        LOGGER.info("[GET rest] delete computer");
+        return computerService.delete(id);
+    }
+}
